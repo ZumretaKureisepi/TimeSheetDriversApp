6490069b07468428f256a2429f456fa13104f8bd
ZumretaKureisepi <zumreta.kuresepi@edu.fit.ba>
2021-03-04 14:30:33 +0100

    Add migration

---
+++ TimeSheetDriversApp/Migrations/20210304132204_Initial.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeSheetDriversApp.WebAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Password = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    PersonalNo = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    FMNumber = table.Column<int>(nullable: false),
+                    RoleId = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    EntryDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    RequestId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfRequest = table.Column<DateTime>(nullable: false),
+                    RequestType = table.Column<int>(nullable: false),
+                    StartDateTime = table.Column<DateTime>(nullable: false),
+                    EndDateTime = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.RequestId);
+                    table.ForeignKey(
+                        name: "FK_Requests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeSheets",
+                columns: table => new
+                {
+                    TimeSheetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<int>(nullable: false),
+                    EntryDate = table.Column<DateTime>(nullable: false),
+                    DayType = table.Column<int>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: true),
+                    EndTime = table.Column<DateTime>(nullable: true),
+                    BreakTime = table.Column<DateTime>(nullable: true),
+                    MetersSquared = table.Column<int>(nullable: true),
+                    KmStand = table.Column<int>(nullable: true),
+                    PrivatTanken = table.Column<int>(nullable: true),
+                    Fuel = table.Column<int>(nullable: true),
+                    AdBlue = table.Column<int>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSheets", x => x.TimeSheetId);
+                    table.ForeignKey(
+                        name: "FK_TimeSheets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebAuthTokens",
+                columns: table => new
+                {
+                    Value = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebAuthTokens", x => x.Value);
+                    table.ForeignKey(
+                        name: "FK_WebAuthTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuditFiles",
+                columns: table => new
+                {
+                    AuditFileId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Field = table.Column<int>(nullable: false),
+                    OldValue = table.Column<DateTime>(nullable: true),
+                    NewValue = table.Column<DateTime>(nullable: true),
+                    DateChanged = table.Column<DateTime>(nullable: false),
+                    ChangedByUserId = table.Column<int>(nullable: false),
+                    TimeSheetId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditFiles", x => x.AuditFileId);
+                    table.ForeignKey(
+                        name: "FK_AuditFiles_Users_ChangedByUserId",
+                        column: x => x.ChangedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuditFiles_TimeSheets_TimeSheetId",
+                        column: x => x.TimeSheetId,
+                        principalTable: "TimeSheets",
+                        principalColumn: "TimeSheetId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuditFiles_ChangedByUserId",
+                table: "AuditFiles",
+                column: "ChangedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuditFiles_TimeSheetId",
+                table: "AuditFiles",
+                column: "TimeSheetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeSheets_UserId",
+                table: "TimeSheets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebAuthTokens_UserId",
+                table: "WebAuthTokens",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditFiles");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "WebAuthTokens");
+
+            migrationBuilder.DropTable(
+                name: "TimeSheets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
